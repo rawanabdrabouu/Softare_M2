66c79f80f01a9e4ca1e42cd064c390d6ddc481ad
daniaahmedd <121130141+daniaahmedd@users.noreply.github.com>
2023-05-26 02:26:23 +0300

    user adds senior request api + admin responds api

--- routes/private/api.js
+++ routes/private/api.js
@@ -114,4 +114,89 @@ module.exports = function (app) {
 });
 })
   
+app.post("/api/v1/senior/request", async function(req, res){//req: nationalID
+  //check if id is empty
+  if(!req){
+    return res.status(400).send("Could send request, the id wasn't sent correctly");
+  }
+  //check if it is a user not an admin
+  const user = await getUser(req);
+  const currentUserId = db.from("users").select('id').where('email', user.email);
+  if(user.isAdmin)
+    return res.status(400).send("An admin can't send a senior request");
+  //check if senior request already exists in table
+  const requestExists = await db
+  .select("*")
+  .from("senior_requests")
+  .where("nationalid", req.body.nationalId)
+  .where("userid", user.id);
+  if (!isEmpty(requestExists)) {
+    return res.status(400).send("request has been sent before");
+  }
+  //check if same user has already enterred their nationalID
+  const userIdExists = await db
+  .select("*")
+  .from("senior_requests")
+  .where("userid", currentUserId);
+  if (!isEmpty(userIdExists)) 
+    return res.status(400).send("This user has already submitted a request before");
+  
+
+  //create new request in table
+
+  const newRequest = {
+    status: 'on-going',
+    userid: currentUserId,
+    nationalid: req.body.nationalId,
+  };
+  try {
+    const request = await db("senior_requests").insert(newRequest).returning("*");
+
+    return res.status(200).json(request);
+  } catch (e) {
+    console.log(e.message);
+    return res.status(400).send("Could not store the request");
+  }
+});
+
+app.put("/api/v1/requests/senior/:requestId", async function(req, res){//req: status of request
+  //all requests with status/req == on-going change their status value to either accepted or rejected
+  //according to their current age
+
+  //1st: check if current user is admin
+  const user = await getUser(req);
+  if(!user.isAdmin){
+    return res.status(400).send("user isn't authorised to do this action");
+  }
+  if(!req){
+    return res.status(400).send("input isn't sent correctly");
+  }
+
+  const requestId = req.params.requestId.substring(1)
+  if(req.body.status == 'accepted' || req.body.status == 'accept'){
+    db.from("senior_requests").where('id', requestId).update({'status': 'accepted'})
+    .then(status => {
+      const newSeniorId = db.from("senior_requests").where('id', requestId).select('userid')
+      db.from("users").where('id', newSeniorId).update({'roleid': 3})
+      .then(roleid => {
+        res.status(200).send("roleid is updated");
+      })
+      .catch(err => {
+        res.status(500).send("error updating user's role id with id = " + user.id);
+      });
+      res.status(200).send("status is updated");
+    })
+    .catch(err => {
+      res.status(500).send("error updating request with id = " + user.id);
+    });
+  }else if(req.body.status == 'rejected' || req.body.status == 'reject'){
+    db.from("senior_requests").where('id', requestId).update({'status': 'rejected'})
+    .then(status => {
+      return res.status(200).send("Status has been updated");
+    })
+    .catch(err => {
+      res.status(500).send("error updating request with user id = " + user.id);
+    });
+  }
+});
 };
