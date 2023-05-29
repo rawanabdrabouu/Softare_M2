06f42516f28c24744e5f724d0431ced41ae1cbf7
daniaahmedd <121130141+daniaahmedd@users.noreply.github.com>
2023-05-29 20:00:48 +0300

    refund request + refund response

--- routes/private/api.js
+++ routes/private/api.js
@@ -28,7 +28,7 @@ const getUser = async function (req) {
 //commment
 module.exports = function (app) {
   // example
-  app.put("/users", async function (req, res) {
+  app.get("/api/v1/users", async function (req, res) {
     try {
       const user = await getUser(req);
       // const {userid}=req.body
@@ -145,7 +145,7 @@ app.post("/api/v1/senior/request", async function(req, res){//req: nationalID
   //create new request in table
 
   const newRequest = {
-    status: 'on-going',
+    status: 'pending',
     userid: currentUserId,
     nationalid: req.body.nationalId,
   };
@@ -160,7 +160,7 @@ app.post("/api/v1/senior/request", async function(req, res){//req: nationalID
 });
 
 app.put("/api/v1/requests/senior/:requestId", async function(req, res){//req: status of request
-  //all requests with status/req == on-going change their status value to either accepted or rejected
+  //all requests with status/req == pending change their status value to either accepted or rejected
   //according to their current age
 
   //1st: check if current user is admin
@@ -173,7 +173,7 @@ app.put("/api/v1/requests/senior/:requestId", async function(req, res){//req: st
   }
 
   const requestId = req.params.requestId.substring(1)
-  if(req.body.status == 'accepted' || req.body.status == 'accept'){
+  if(req.body.seniorStatus == 'accepted' || req.body.seniorStatus == 'accept'){
     db.from("senior_requests").where('id', requestId).update({'status': 'accepted'})
     .then(status => {
       const newSeniorId = db.from("senior_requests").where('id', requestId).select('userid')
@@ -189,7 +189,7 @@ app.put("/api/v1/requests/senior/:requestId", async function(req, res){//req: st
     .catch(err => {
       res.status(500).send("error updating request with id = " + user.id);
     });
-  }else if(req.body.status == 'rejected' || req.body.status == 'reject'){
+  }else if(req.body.seniorStatus == 'rejected' || req.body.seniorStatus == 'reject'){
     db.from("senior_requests").where('id', requestId).update({'status': 'rejected'})
     .then(status => {
       return res.status(200).send("Status has been updated");
@@ -199,4 +199,111 @@ app.put("/api/v1/requests/senior/:requestId", async function(req, res){//req: st
     });
   }
 });
+
+app.post("/api/v1/refund/:ticketId", async function(req, res){
+  //go to table refund requests and send a request for response
+  const user = await getUser(req);
+  const currentUserId = db.from("users").select('id').where('email', user.email);
+
+  if(user.isAdmin)
+    return res.status(400).send("An admin can't send a refund request");
+
+  //check if user sent refund request for the same ticket
+  const ticketId = req.params.ticketId.substring(1)
+  const refRequestExists = await db
+  .select("*")
+  .from("refund_requests")
+  .where("userid", currentUserId)
+  .where("ticketid", ticketId);
+  if (!isEmpty(refRequestExists)) {
+    return res.status(400).send("request has been sent before");
+  }
+
+  //check if it's a future dated ticket
+  var tripDateDb = db.from("tickets").select('tripdate').where('id', ticketId);
+  var stringTripDate = tripDateDb.toString()
+  var tripYear = stringTripDate.substring(0, 4)
+  var tripMonth = stringTripDate.substring(5, 7)
+  var tripDay = stringTripDate.substring(8, 10)
+  var tripDate = new Date(tripYear, tripMonth, tripDay)
+  var myDate = new Date();
+  var myMonth = myDate.getMonth()+1;
+  var myDay = myDate.getDay();
+  var myYear = myDate.getFullYear();
+  var myDate = new Date(myYear, myMonth, myDay);
+  if(myDate < tripDate){
+    return res.status(400).send("ticket isn't future dated");
+  }
+  //transactions table to get refund amount
+  const payPlan = db.from("transactions").select('purchasetype').where('userid', currentUserId);
+  const payPlanStr = payPlan.toString()
+  if(payPlanStr.includes('subscription')){
+    var amountPayed = 0;
+  }else{
+    var amountPayed = db.from("transactions").select('amount').where('userid', currentUserId);
+  }
+  const newRefRequest = {
+    status: 'pending',
+    userid: currentUserId,
+    refundamount: amountPayed,
+    ticketid: ticketId,
+  };
+  try {
+    const refRequest = await db("refund_requests").insert(newRefRequest).returning("*");
+
+    return res.status(200).json(refRequest);
+  } catch (e) {
+    console.log(e.message);
+    return res.status(400).send("Could not store the request");
+  }
+});
+
+app.put("/api/v1/requests/refunds/:requestId", async function(req, res){
+  const user = await getUser(req);
+  // const currentUserId = await db.from("users").select('id').where('email', user.email);
+  
+  if(!req){
+    return res.status(400).send("Couldn't send request, refund status wasn't sent correctly");
+  }
+  if(!user.isAdmin)
+    return res.status(400).send("A user can't approve/reject a refund request");
+
+  const requestId = req.params.requestId
+  const ticketId = await db.from("refund_requests").where('id', requestId).select('ticketid','userid').first()
+  const subsid = await db.from("tickets").where('id', ticketId.ticketid).select('subid').first();
+  const payPlan = await db.from("transactions").select('purchasetype').where('purchasedid',ticketId.ticketid).first();
+  const payPlanStr = payPlan.purchasetype
+
+  if(req.body.refundStatus == 'accepted' || req.body.refundStatus == 'accept'){
+    await db.from("refund_requests").where('status', 'pending').update({status: 'accepted'})
+      //tickets and rides table ha-delete menhom el ride el refunded
+    //await db.from("tickets").del().where('id', ticketId).where('userid', currentUserId)
+    await db.from("rides").del().where('ticketid', ticketId.ticketid).where('userid', ticketId.userid)
+    if(payPlanStr == 'ticket'){
+      await db.from("transactions").del().where('purcchaseid', ticketId.ticketid)
+    }
+    //ADD 7TET ADDING TICKETS OF SUBSCRIPTION IF USER IS SUBSCRIBED LAW LAA YB2A 5ALAS
+    if(payPlanStr.includes('subscription')){
+      console.log('here')
+      const nooftickets = await db.from("subsription").where('id', subsid.subid).select('nooftickets').first()
+      const newnotickets = nooftickets.nooftickets 
+      await db.from("subsription").where('id', subsid.subid).update({'nooftickets': newnotickets+ 1})
+      .then(status => {
+        res.status(200).send("refund request status is updated");
+      })
+      .catch(err => {
+        res.status(400).send("error updating request with id = " + requestId);
+      })
+    }else{
+      res.status(200).send("refund request status is updated");
+    }
+  }else if(req.body.status == 'rejected' || req.body.status == 'reject'){
+    await db.from("refund_requests").where('id', requestId).update({'status': 'rejected'})
+    .then(status =>{
+      return res.status(200).send("Status has been updated");
+    })
+  }
+
+})
+
 };
